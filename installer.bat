eddd365720809031ddafa985ff36459728a41ba3
NatsIsHereeeeee <68655260+NatsIsHereeeeee@users.noreply.github.com>
2020-11-25 17:31:23 +0100

    de

---
+++ installer.bat
@@ -0,0 +1,3 @@
+@echo off
+title NATS SELFBOT Installer
+npm install discord.js@11.6.4 chalk discord-webhook-node
---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+title NATS SELFBOT
+:top
+cls
+node .
+cd C:\Program Files (x86)\Google\Chrome\Application
+start chrome.exe https://www.youtube.com/channel/UC7KMFrYHQzmXCbomARoP9zg https://github.com/NatsIsHereeeeee
+pause
+cd C:\Program Files (x86)\Mozilla Firefox
+start firefox.exe https://www.youtube.com/channel/UC7KMFrYHQzmXCbomARoP9zg https://github.com/NatsIsHereeeeee
+goto :top
